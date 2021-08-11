9f3310178ec54b082ac99fe3dc850ad6cecaa076
shawn2776 <shawn.harrington2776@gmail.com>
2021-08-11 15:16:18 -0700

    "base proj started"

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log('Welcome to the UFO Custom Covers Project');
+require('dotenv').config(); // inject fake env vars
+const { PORT } = require('./config');
+
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+
+const server = express();
+
+server.use(express.json()); // teaches express to parse req.body
+server.use(cors());
+server.use(helmet());
+
+server.get('/', (req, res) => {
+  res.send(`
+  <h1>Welcome to UFO Custom Covers</h1>
+  <p>Custom Sanded, Polished, and Etched Engine Covers</p>
+  `)
+})
+
+server.use('*', (req, res, next) => {
+  res.json({
+    message: 'Welcome to UFO Custom Covers'
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`listening on ${PORT}`)
+});
